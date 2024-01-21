fa172ccd24ee5caa7f2aeb94757fc4a9795bff71
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-01-21 23:02:06 +0000

    task 5

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-  let arr1 = [...array1, ...array2, ...string.split('')];
+  const arr1 = [...array1, ...array2, ...string.split('')];
   return (arr1);
 }
