1b21a334f73e19910e9f078441775b92eb534489
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-01-23 00:11:37 +0000

    task 0 fixed

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,9 @@
 export default function getResponseFromAPI() {
-  return new promise(() => {});
+  return new Promise((resolve, reject) => {
+    if (1+1 <= 3) {
+      resolve(3);
+    } else {
+      reject(`false`);
+    }
+  });
 }
