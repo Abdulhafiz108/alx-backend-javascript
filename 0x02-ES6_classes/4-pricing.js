6ea8c3f82311e62e3734b89b579877f0b729249f
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-01-28 23:20:24 +0000

    task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,16 +1,16 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-  constructor(amount, Currency) {
+  constructor(amount, currency) {
     this._amount = amount;
-    this._currency = Currency;
+    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  get Currency() {
+  get currency() {
     return this._currency;
   }
 
@@ -18,7 +18,7 @@ export default class Pricing {
     this._amount = value;
   }
 
-  set Currency(value) {
+  set currency(value) {
     this._currency = value;
   }
 
