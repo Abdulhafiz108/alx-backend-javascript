ce2f8525fd3eb9ef9fd89c8416d76e6147aefc21
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-01-31 20:48:07 +0000

    task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
+  const int8View = new DataView(buffer, 0);
 
   if (position > length - 1){
     throw new Error('Position outside range')
   }
-  int8View[position] = value;
+  int8View.setInt8(position, value);
   return int8View;
 }
