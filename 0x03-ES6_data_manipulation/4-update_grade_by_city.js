c142832ec1cd27ffd9fa3959e845ead09863bcf0
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-01-31 18:02:33 +0000

    task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const defaultgrade = 'N/A'
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => ({
+      id: student.id,
+      firstName: student.firstName,
+      location: student.location,
+      grade: (newGrades
+	 .filter((grade) => grade.studentId === student.id)
+	 .pop() || defaultgrade).grade,
+      }));
+}
