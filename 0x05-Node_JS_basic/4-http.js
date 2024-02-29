3f66c98576ef1b81a541283211c7d30f85ba6118
Abdulhafiz Gazali Ado <ahgazali108@gmail.com>
2024-02-29 20:54:17 +0000

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,16 @@
 const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 1245;
+
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
-const PORT = 1245;
-const HOST = '127.0.0.1';
-
-app.listen(PORT, HOST, () => {
-  console.log('Server running at http://${HOST}:${PORT}/');
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
